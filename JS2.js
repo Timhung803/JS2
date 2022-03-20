172a4bec2cbebbdbf0c1d6d634867eb4f42df665
Timhung803 <timhkt@gmail.com>
2022-03-21 02:20:25 +1300

    First String

---
+++ JS2.js
@@ -0,0 +1,10 @@
+let checking = 'JS2';
+console.log(checking);
+
+// String
+// String primitive
+const message = 'This is my first message';
+
+
+// String object
+const another = new String('hi');
