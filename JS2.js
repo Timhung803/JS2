d81cf9850aea8777328cd19912b2e671d46f90ae
Timhung803 <timhkt@gmail.com>
2022-03-24 01:06:32 +1300

    if....else

--- JS2.js
+++ JS2.js
@@ -42,3 +42,18 @@ function greeting (Owners) {
 
 greeting('Tim');
 
+//if ..... else
+// Hour
+// If hour is between 6 am and 12 pm: Good morning!
+// If hour is between 12 pm and 6 pm: Good afternoon!
+// Otherwise: Good evening!
+
+let hour = 12;
+
+if (hour >= 6 && hour < 12)
+    console.log('Good morning!');
+else if (hour >=12 && hour < 18)
+    console.log('Good afternoon!');
+else 
+    console.log('Good evening!');
+
