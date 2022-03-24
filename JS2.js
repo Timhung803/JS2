6c137e4d9e501ca876944a07fda168c7dc3b874c
Timhung803 <timhkt@gmail.com>
2022-03-25 02:18:55 +1300

    Loops - For

--- JS2.js
+++ JS2.js
@@ -57,3 +57,15 @@ else if (hour >=12 && hour < 18)
 else 
     console.log('Good evening!');
 
+//Loops - For
+// for (initialExpression; condition; incrementExpression)
+// for (let i = 0; i < 5; i++) {
+//     console.log('Hello World', i);
+// }
+
+// for (let i = 1; i <=5; i++) {
+//     if (i % 2 !== 0) console.log(i);
+// }
+
+for (let i = 5; i >= 1; i--)
+    if (i % 2 !==0) console.log(i);
