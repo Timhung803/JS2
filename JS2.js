a3075737d63a7e8c290afe6c45a90a101dc64aa3
Timhung803 <timhkt@gmail.com>
2022-03-26 03:06:40 +1300

    Factory Function

--- JS2.js
+++ JS2.js
@@ -69,3 +69,31 @@ else
 
 for (let i = 5; i >= 1; i--)
     if (i % 2 !==0) console.log(i);
+
+// Factory Functions //
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     isVisible: true,
+//     draw: function() {
+//         console.log('draw');
+//     }
+// };
+
+function createCircle (radius) {
+    return {
+        radius,
+        draw() {
+            console.log('Draw this circle');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+const circle2 = createCircle(2);
+console.log(circle2);
+circle1.draw();
