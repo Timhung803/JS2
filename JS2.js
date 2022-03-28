6574a79c0630864bc3aeb05d2af6444fb7b86943
Timhung803 <timhkt@gmail.com>
2022-03-29 00:49:06 +1300

    Value tpye vs. Reference tyoe

--- JS2.js
+++ JS2.js
@@ -149,3 +149,29 @@ person.location = 'Japan Yokohama';
 console.log(person);
 // console.log(`${person.firstName} ${person.lastName}`);
 
+// Value Type vs. Reference Type
+let x = {value: 10};
+let y = x;
+
+x.value = 20;
+
+// Example 1
+ let number = 10;
+
+ function increase(number) {
+     number++;
+ }
+
+ increase(number);
+ console.log(number);
+
+// Example 2
+
+let obj = {value: 10};
+
+function increase(obj) {
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj);
